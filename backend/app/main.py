d26565ae2707697ece8f050d0130e8363578eb96
FregEscape3 <jbie1@hwemail.com>
2026-08-11 17:45:17 -0700

    backend ender

--- backend/app/main.py
+++ backend/app/main.py
@@ -90,7 +90,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://prism-dashboard-gw9y.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
